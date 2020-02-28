123a3202687cd261d4b49f6c2b20d079efcb8566
Nick Sagona <nick@nolainteractive.com>
2020-02-28 12:39:56 -0600

    Add html static path

--- conf.py
+++ conf.py
@@ -4,6 +4,8 @@ from pygments.lexers.web import PhpLexer
 
 sys.path.append(os.path.abspath('_exts'))
 
+html_static_path = ['_html']
+
 extensions = []
 master_doc = 'index'
 highlight_language = 'php'
