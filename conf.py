09578592c89eaf8e0d52cdc65d1222e718519a30
Nick Sagona <nick@nolainteractive.com>
2020-02-28 12:58:05 -0600

    Add robots

--- conf.py
+++ conf.py
@@ -4,7 +4,7 @@ from pygments.lexers.web import PhpLexer
 
 sys.path.append(os.path.abspath('_exts'))
 
-html_extra_path = ['_html']
+html_static_path = ['_static', '_static/robots.txt']
 
 extensions = []
 master_doc = 'index'
